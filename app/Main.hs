797a58134396160575deb74783bc8565b3f69e9f
James Carlson <carlson@jxxM3.local>
2023-11-27 23:50:21 -0500

    Upgrade fileserver:
      - the path /list lsits the directory as a page of active links
      - the command 'stack run' accepts two mandatory arguments: the port number and teh server's root directory

--- app/Main.hs
+++ app/Main.hs
@@ -3,23 +3,70 @@ module Main where
 
 import Snap
 import Snap.Http.Server (httpServe, defaultConfig, setPort)
-import Snap.Core (route)
+import Snap.Core (route, modifyResponse, setContentType)
 import Snap.Util.FileServe (serveDirectory)
 import qualified Data.ByteString.Char8 as B
-import System.Directory (listDirectory)
+import System.FilePath (takeExtension)
+import System.Directory (listDirectory, doesFileExist)
 import Control.Monad.IO.Class (liftIO)
+import System.Environment (getArgs)
+import Text.Read (readMaybe)
 
 main :: IO ()
-main = httpServe (setPort 8111 defaultConfig) site
+main = do
+         args <- getArgs
+         case args of 
+            (port_:serverDirectory:[]) -> 
+                case readMaybe port_ of 
+                    Nothing -> putStrLn "First argument (port) can't be converted to an integer"
+                    Just port -> 
+                        httpServe (setPort port defaultConfig) (site serverDirectory)
+            _ -> putStrLn "command takes exactly to args, <port> and <server root directory>"
 
-site :: Snap ()
-site = route [( (B.pack "list"), listFilesHandler)
-               , (B.pack "", serveDirectory ".")]
+site :: String -> Snap ()
+site serverDirectory = route [(B.pack "list", (listFilesHandler serverDirectory))
+               , (B.pack "", fileHandler)]
  
- -- Handler to list files in the current directory
-listFilesHandler :: Snap ()
-listFilesHandler = do
-    files <- liftIO (listDirectory ".")
-    let fileList =  B.pack $ unlines files
-    writeBS fileList
+--- Handler to list files in the current directory as clickable links
+listFilesHandler :: String -> Snap ()
+listFilesHandler serverDirectory = do
+    files <- liftIO (listDirectory serverDirectory)
+    let links = map makeLink files
+    let html = B.pack $ "<html><body>" ++ unlines links ++ "</body></html>"
+    modifyResponse $ setContentType  $ B.pack "text/html; charset=utf-8"
+    writeBS html
 
+-- Function to create an HTML link for a file that opens in a new tab/window
+makeLink :: String -> String
+makeLink file = "<a href='/" ++ file ++ "'>" ++ file ++ "</a><br>"
+
+
+fileHandler :: Snap ()
+fileHandler = do
+    rq <- getRequest
+    let uri = rqURI rq
+    let filePath = "." ++ B.unpack uri  -- Constructing the file path from URI
+    exists <- liftIO $ doesFileExist filePath
+    if exists then
+        case takeExtension filePath of
+            ".txt" -> serveAsText filePath
+            ".yaml" -> serveAsText filePath
+            ".md" -> serveAsText filePath
+            ".hs" -> serveAsText filePath
+            ".pdf" -> serveAsPDF filePath
+
+            _      -> pass  -- Fall back to default behavior
+    else
+        pass  -- Fall back to default behavior
+
+serveAsText :: FilePath -> Snap ()
+serveAsText filePath = do
+    fileContents <- liftIO $ B.readFile filePath
+    modifyResponse $ setContentType $ B.pack "text/plain; charset=utf-8"
+    writeBS fileContents
+
+serveAsPDF :: FilePath -> Snap ()
+serveAsPDF filePath = do
+    fileContents <- liftIO $ B.readFile filePath
+    modifyResponse $ setContentType $ B.pack "application/pdf"
+    writeBS fileContents
