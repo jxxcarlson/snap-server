8e550007fcbc055a96cf87dbec8a12eb51b62358
James Carlson <jxxcarlson@gmail.com>
2023-12-09 02:30:23 -0500

    Modify server so as to accept POST requests.

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import Snap
+import qualified Snap as S
 import Snap.Http.Server (httpServe, defaultConfig, setPort)
 import Snap.Core (route, modifyResponse, setContentType)
 import Snap.Util.FileServe (serveDirectory, serveFile)
+import Snap.Util.CORS (CORSOptions(..), HashableMethod(..), OriginList(Origins), applyCORS, mkOriginSet)
 import qualified Data.ByteString.Char8 as B
 import System.FilePath (takeExtension, takeFileName, (</>))
 import System.Directory (listDirectory, doesFileExist, doesDirectoryExist)
 import Control.Monad.IO.Class (liftIO)
 import System.Environment (getArgs)
 import Text.Read (readMaybe)
+import Control.Applicative ((<|>))
+import Data.CaseInsensitive (mk)
+import qualified Data.HashSet as HashSet
+import Network.URI (parseURI)
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Text as T
+
+
+
+
+allowedOrigins :: [String]
+allowedOrigins =
+  [ "http://localhost:8000"
+  ]
+
 
 main :: IO ()
 main = do
@@ -23,47 +41,57 @@ main = do
                         httpServe (setPort port defaultConfig) (site serverDirectory)
             _ -> putStrLn "command takes exactly two args, <port> and <server root directory>"
 
-site :: String -> Snap ()
-site serverDirectory = route [ (B.pack "", fileAndDirectoryHandler) ]
- 
 
-fileAndDirectoryHandler :: Snap ()
-fileAndDirectoryHandler = do
-    rq <- getRequest
-    let dirPath = "." </> B.unpack (rqPathInfo rq)
-    isDir <- liftIO $ doesDirectoryExist dirPath
+site :: String -> S.Snap ()
+site serverDirectory = route [ (B.pack "", S.method S.OPTIONS handleOptions <|> fileAndDirectoryHandler), (B.pack "postdata", S.method S.POST handlePost)]
+  
+
+handleOptions :: S.Snap ()
+handleOptions = 
+    allow S.GET allowedOrigins $ do
+    --setCorsHeaders
+    modifyResponse $ S.setResponseCode 200
+    S.writeBS$ B.pack ""  
+
+fileAndDirectoryHandler :: S.Snap ()
+fileAndDirectoryHandler = 
+    allow S.GET allowedOrigins $ do
+    rq <- S.getRequest
+    let dirPath = "." </> B.unpack (S.rqPathInfo rq)
     isFile <- liftIO $ doesFileExist dirPath
+    isDir <- liftIO $ doesDirectoryExist dirPath
     if isDir
         then do
             contents <- liftIO $ listDirectory dirPath
             let links = map (makeLink dirPath) contents
             let html = B.pack $ "<html><body>" ++ unlines links ++ "</body></html>"
             modifyResponse $ setContentType $ B.pack "text/html; charset=utf-8"
-            writeBS html
+            S.writeBS html
     else if isFile 
         then do
             serveResource dirPath
     else
-            writeBS $ B.pack "Not a directory or directory does not exist."
+            S.writeBS$ B.pack "Not a directory or directory does not exist."
 
 makeLink :: FilePath -> String -> String
 makeLink dirPath file = "<a href='" ++ dirPath ++ "/" ++ file ++ "'>" ++ file ++ "</a><br>"
 
 
-serveAsText :: FilePath -> Snap ()
+serveAsText :: FilePath -> S.Snap ()
 serveAsText filePath = do
     fileContents <- liftIO $ B.readFile filePath
     modifyResponse $ setContentType $ B.pack "text/plain; charset=utf-8"
-    writeBS fileContents
+    S.writeBS fileContents
 
-serveAsPDF :: FilePath -> Snap ()
+serveAsPDF :: FilePath -> S.Snap ()
 serveAsPDF filePath = do
     fileContents <- liftIO $ B.readFile filePath
     modifyResponse $ setContentType $ B.pack "application/pdf"
-    writeBS fileContents
+    S.writeBS fileContents
 
-serveResource :: FilePath -> Snap ()
+serveResource :: FilePath -> S.Snap ()
 serveResource filePath = do
+    -- setCorsHeaders
     let ext = takeExtension filePath
     case ext of
         ".png"  -> serveWithMimeType (B.pack "image/png") filePath
@@ -75,11 +103,76 @@ serveResource filePath = do
         ".md"   -> serveAsText filePath
         ".hs"   -> serveAsText filePath
         ".elm"  -> serveAsText filePath
+        ".csv"  -> serveAsText filePath
         ".pdf"  -> serveAsPDF filePath
         _       -> serveAsText filePath
+    S.writeBS"Data read successfully"
 
 
-serveWithMimeType :: B.ByteString -> FilePath -> Snap ()
+serveWithMimeType :: B.ByteString -> FilePath -> S.Snap ()
 serveWithMimeType mimeType filePath = do
     modifyResponse $ setContentType mimeType
     serveFile filePath
+
+
+-- CORS
+
+setCorsHeaders :: S.Snap ()
+setCorsHeaders = modifyResponse $ do
+    S.addHeader (mk $ B.pack "Access-Control-Allow-Origin") (B.pack "*")
+    S.addHeader (mk $ B.pack "Access-Control-Allow-Methods") (B.pack "GET, POST, OPTIONS")
+    S.addHeader (mk $ B.pack "Access-Control-Allow-Headers") (B.pack "Origin, Accept, Content-Type")
+
+
+
+allow :: S.Method -> [String] -> S.Snap () -> S.Snap ()
+allow method_ origins snap = 
+  applyCORS (toOptions method_ origins) $ S.method method_ snap
+
+toOptions :: (Monad m) => S.Method -> [String] -> CORSOptions m
+toOptions method_ origins =
+  let
+    allowedOrigins = toOriginList origins
+    allowedMethods = HashSet.singleton (HashableMethod method_)
+  in
+  CORSOptions
+    { corsAllowOrigin = return allowedOrigins
+    , corsAllowCredentials = return True
+    , corsExposeHeaders = return HashSet.empty
+    , corsAllowedMethods = return allowedMethods
+    , corsAllowedHeaders = return
+    }
+
+
+toOriginList :: [String] -> OriginList
+toOriginList origins =
+  Origins $ mkOriginSet $
+    case traverse parseURI origins of
+      Just uris -> uris
+      Nothing -> error "invalid entry given to toOriginList list"
+
+-- POST Data
+
+data PostData = PostData
+  { path :: FilePath,
+    content :: String
+  }
+
+instance Aeson.FromJSON PostData where
+  parseJSON = Aeson.withObject "PostData" $ \v -> PostData
+    <$> v Aeson..: "path"
+    <*> v Aeson..: "content"
+
+handlePost :: S.Snap ()
+handlePost = 
+    allow S.POST allowedOrigins $ do
+    S.writeBS "Begin processing S.POST request "
+    modifyResponse $ S.setHeader "Content-Type" "application/json"
+    body <- S.readRequestBody 1000000 -- Max size of the request body
+    case Aeson.decode body of
+        Just postData -> do
+            liftIO $ writeFile (path postData) (content postData)
+            S.writeBS   "Data written successfully"
+        Nothing -> do
+            modifyResponse $ S.setResponseCode 400
+            S.writeBS "Invalid JSON data"
